8b4ce7e8b4a64f96edf9c1d1cca6b5df1e7325e4
Pablo Jabat <pablojabat@gmail.com>
2018-10-02 12:14:19 +0200

    added .conf file for MapMatchingApp.scala

--- build.sbt
+++ build.sbt
@@ -7,3 +7,5 @@ scalaVersion := "2.11.8"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
 
 libraryDependencies += "net.liftweb" %% "lift-json" % "2.6+"
+
+libraryDependencies += "com.typesafe" % "config" % "1.3.2"
--- src/main/scala/com/trafficmonitoring/MapMatchingApp.scala
+++ src/main/scala/com/trafficmonitoring/MapMatchingApp.scala
@@ -2,6 +2,8 @@ package com.trafficmonitoring
 
 import java.io.PrintWriter
 
+import com.typesafe.config.ConfigFactory
+
 import org.apache.spark.sql.SparkSession
 import com.geo.elements._
 import com.geo.algorithms.MapMatching._
@@ -15,14 +17,11 @@ object MapMatchingApp {
     *
     * args:
     *
-    * This app takes some input GPS data and road data coming from OpenStreetMaps and carries out the map matching
+    * All the arguments needed for this app have to be set in application.conf
     *
-    * At this point we provide the location of the road data and input GPS data inside the code
     */
 
   def main(args: Array[String]): Unit = {
-
-    println("Traffic Monitoring")
 
     val spark = SparkSession.builder
       .appName(s"Traffic Monitoring")
@@ -35,14 +34,41 @@ object MapMatchingApp {
     import spark.implicits._
 
     println("======================================")
-    println("|         Simple Map Matching        |")
+    println("|          Map Matching App          |")
     println("======================================")
+
+    //We load all the parameters needed in the application fromm the configuration file
+
+    val maxLat = ConfigFactory.load().getDouble("boxlimits.maxLat")
+
+    val minLat = ConfigFactory.load().getDouble("boxlimits.minLat")
+
+    val maxLon = ConfigFactory.load().getDouble("boxlimits.maxLon")
+
+    val minLon = ConfigFactory.load().getDouble("boxlimits.minLon")
+
+    val resolution = ConfigFactory.load().getDouble("grid.resolution")
+
+    val clearance = ConfigFactory.load().getDouble("grid.clearance")
+
+    val radius = ConfigFactory.load().getDouble("grid.radius")
+
+    val gpsDataPath = ConfigFactory.load().getString("data.gps")
+
+    val mapDataPath = ConfigFactory.load().getString("data.map")
+
+    val outputPath = ConfigFactory.load().getString("data.output")
+
+    val stdev_deltaPhi = ConfigFactory.load().getDouble("stdev.deltaPhi")
+
+    val stdev_b = ConfigFactory.load().getDouble("stdev.b")
+
 
     //We first set the region on the map in which we want to perform map matching
 
-    val osmBox = BoxLimits(40.647, 40.643, 22.937, 22.933)
+    val osmBox = BoxLimits(maxLat, minLat, maxLon, minLon)
 
-    val myGrid = new Grid(osmBox,150,100)
+    val myGrid = new Grid(osmBox, resolution, clearance, radius)
 
     println("Number of latitude divisions: " + myGrid.latDivisions)
 
@@ -50,15 +76,13 @@ object MapMatchingApp {
 
     //We load and index the GPS data
 
-    val gpsData = loadGPSPointsSpark(sc, "/home/pablo/DE/DataSets/testDataSets/input_gps_08122017.csv")
+    val gpsData = loadGPSPointsSpark(sc, gpsDataPath)
 
     val gpsDataIndexed = filterIndexGPSPointsSpark(gpsData, myGrid)
 
     //We load and index and group the Ways data
 
-    val waysData = loadMapSpark(sc, "/home/pablo/DE/DataSets/testDataSets/mapcsv.csv")
-
-    println("number of ways " + waysData.count())
+    val waysData = loadMapSpark(sc, mapDataPath)
 
     val waysDataIndexed = filterIndexMapSpark(waysData, myGrid)
 
@@ -68,9 +92,11 @@ object MapMatchingApp {
 
     val cells = waysDataIndexed.take(10).toList
 
-    val pw1 = new PrintWriter("/home/pablo/DE/GeoJSON/cellWaysRDD.json")
+    val pw1 = new PrintWriter(outputPath + "/cellWaysRDD.json")
 
     cellsToJSON(pw1,cells,myGrid)
+
+    println("cellWaysRDD.json created")
 
     waysDataIndexed.unpersist()
 
@@ -84,16 +110,18 @@ object MapMatchingApp {
 
     val sample = mergedData.collect.toList
 
-    val pw2 = new PrintWriter("/home/pablo/DE/GeoJSON/mergedDataRDD.json")
+    val pw2 = new PrintWriter(outputPath + "/mergedDataRDD.json")
 
     indexedDataToJSON(pw2,sample,myGrid)
+
+    println("mergedDataRDD.json created")
 
     mergedData.unpersist()
 
     //The last step is to pass the resulting RDD to the map matching algorithm
 
     val matchedData = mergedData
-      .map{case (p: Point, waysLst: List[Way]) => (naiveBayesClassifierMM(p,waysLst,1,1),p,waysLst)}
+      .map{case (p: Point, waysLst: List[Way]) => (naiveBayesClassifierMM(p,waysLst,stdev_b,stdev_deltaPhi),p,waysLst)}
       .map{case ((way, new_p), p, ways) => (way, p, new_p, ways)}
 
     //Once we carry out the Map Matching computation we write the results in a csv
@@ -102,20 +130,26 @@ object MapMatchingApp {
 
     matchedData.map{case (way, p, new_p, _) => (way.osmID, findCarID(p.id), findTimestamp(p.id), p.lat, p.lon, p.orientation, new_p.lat, new_p.lon)}
       .toDF("wayID","carID","timestamp","latitude","longitude","orientation","matched latitude","matched longitude")
-      .coalesce(1).write.csv("/home/pablo/DE/DataSets/testDataSets/results")
+      .coalesce(1).write.csv(outputPath + "/MMresults")
 
     matchedData.unpersist()
+
+    println("MMresults folder containing matched data was created")
 
     //We can also write some results in geojson format for visualization purposes
 
     val someResults = matchedData.collect.toList
 
-    val pw3 = new PrintWriter("/home/pablo/DE/GeoJSON/MMResultsRDD.json")
+    val pw3 = new PrintWriter(outputPath + "/MMResultsRDD.json")
 
     val jsonResults = someResults
       .map(a => (a._2, a._3, a._4))
 
     resultsToJSON(pw3, jsonResults, myGrid)
+
+    println("MMResultsRDD.json created")
+
+    println("Map Matching App successfully finished running")
 
   }
 
